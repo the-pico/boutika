3124e4ec2b165bb1bffe4ab0e99cfebe37804ec6
bassiry omar <bassiryomar@gmail.com>
2017-10-18 10:00:54 +0100

    adding missing project folders

--- boutika-web/src/main/java/com/oby/autumn/boutika/web/configuration/WebConfig.java
+++ boutika-web/src/main/java/com/oby/autumn/boutika/web/configuration/WebConfig.java
@@ -3,7 +3,6 @@ package com.oby.autumn.boutika.web.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
